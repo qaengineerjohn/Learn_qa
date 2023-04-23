79fabeff2d07dfb53869fc32e310e94a3afa14bd
johnlitvinov <john.litvinov@gmail.com>
2023-04-23 12:02:09 +0200

    added testHelloFromIvan(); added first restassured Test;

---
+++ src/test/java/HelloFromIvan.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloFromIvan {
+
+    @Test
+    public void testHelloFromIvan() {
+       System.out.println("Hello from Ivan");
+    }
+
+}
