a6ee4c8851e55dbb956971012a46344d00200469
johnlitvinov <john.litvinov@gmail.com>
2023-04-26 17:48:05 +0200

    added ex7;

---
+++ src/test/java/ex7.java
@@ -0,0 +1,36 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ex7 {
+
+    @Test
+    public void ex7() {
+
+        String URL = "https://playground.learnqa.ru/api/long_redirect";
+
+        do {
+            Response response = given()
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(URL)
+                    .andReturn();
+
+            int statusCode = response.getStatusCode();
+            if (statusCode == 200) {
+                System.out.println("Успешный запрос!");
+                System.out.println(statusCode);
+                System.out.println(response.getHeader("Location"));
+                break;
+            } else {
+                // Получаем URL для редиректа из нужного заголовка
+                URL = response.getHeader("Location");
+                System.out.println(statusCode);
+                System.out.println(response.getHeader("Location"));
+            }
+        } while (URL != null);
+    }
+}
