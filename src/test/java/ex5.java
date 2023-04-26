31968fff2fd6dde1758bf756c2f7406285f65e04
johnlitvinov <john.litvinov@gmail.com>
2023-04-26 16:27:30 +0200

    added ex5;

---
+++ src/test/java/ex5.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class ex5 {
+
+    @Test
+    public void ex5() {
+
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String answer = response.get("messages[1].message");
+        System.out.println(answer);
+    }
+}
