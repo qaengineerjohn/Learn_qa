403fd29f30fe9f1306769c1455774bacca3e8bf8
johnlitvinov <john.litvinov@gmail.com>
2023-05-14 12:36:29 +0200

    added ex14 added classes: Assertions,BaseTestCase,UserAuthTest

---
+++ src/test/java/lib/BaseTestCase.java
@@ -0,0 +1,30 @@
+package lib;
+
+import io.restassured.response.Response;
+import io.restassured.http.Headers;
+import java.util.Map;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BaseTestCase {
+
+    protected String getHeader(Response Response, String name){
+        Headers headers = Response.getHeaders();
+
+        assertTrue(headers.hasHeaderWithName(name),"Response doesn't have headers " + name);
+        return headers.getValue(name);
+    }
+
+    protected String getCookie (Response Response, String name){
+        Map<String,String> cookies = Response.getCookies();
+
+        assertTrue(cookies.containsKey(name),"Response doesn't have cookies " + name);
+
+        return cookies.get(name);
+    }
+
+    protected int getIntFromJson(Response Response, String name){
+        Response.then().assertThat().body("$",hasKey(name));
+        return Response.jsonPath().getInt(name);
+    }
+}
